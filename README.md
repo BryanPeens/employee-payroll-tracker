1798d70d545af77e30443d6b04b22e452fb2ab3a
Bryan Peens <peensbryan1@gmail.com>
2024-03-14 02:11:48 -0400

    first commit, added script and readme

---
+++ README.md
@@ -0,0 +1,43 @@
+# Employee Payroll Tracker
+
+## Table of Contents
+
+- [Description](#description)
+- [Features](#features)
+- [Expectation](#expectation)
+- [License](#license)
+- [Contact](#contact)
+
+## Description
+
+This project is a simple web application that allows users to collect, display, and interact with employee data using JavaScript, HTML, and CSS.
+
+## Main Features:
+
+- Collecting Employee Data: Users can input employee details such as first name, last name, and salary through prompts.
+
+- Displaying Average Salary: The application calculates and displays the average salary of all entered employees.
+
+- Selecting Random Employee: Users can randomly select and display an employee from the entered data.
+
+- Displaying Employee Data: The application presents the entered employee data in an HTML table, including first name, last name, and salary.
+
+The project utilizes JavaScript for logic implementation, HTML for structure, and CSS for styling. It employs event listeners to trigger actions like collecting employee data when the "Add Employees" button is clicked. Additionally, it features error handling to prevent null elements when displaying the average salary.
+
+## Expected Output:
+
+### Browser Output
+- In the web browser, the user is prompted to create an employee object
+- The displayed output should be as follows
+
+![screenshot-1](./Develop/assets/images/screenshot-1.png)
+
+### Dev Tools Output
+- In the dev tools, you should see the following after creating an employee using a prompt
+
+![screenshot-2](./Develop/assets/images/screenshot-2.png)
+
+## License and Usage:
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+## [Contact Me](mailto:peensbryan75@gmail.com)
---
+++ develop/assets/script.js
@@ -0,0 +1,136 @@
+// Get a reference to the #add-employees-btn element
+const addEmployeesBtn = document.querySelector("#add-employees-btn");
+
+// ========================================================================== Get Employees
+// Collect employee data
+const collectEmployees = function () {
+  // TODO: Get user input to create and return an array of employee objects
+  let employees = []; // Initialize an empty array to store employee data
+
+  // Loop until the user decides to end it
+  while (true) {
+    let firstName = prompt("Enter first name (or type 'done' to finish):"); // Prompt user to enter name
+    if (firstName.toLowerCase() === "done") break; // Exit loop if user enters 'done'
+
+    let lastName = prompt("Enter last name:"); // Prompt user to enter lastname
+    let salary = prompt("Enter salary:"); // Prompt user to enter salary
+
+    // Create an object for the employee, using the data a user input
+    let employee = {
+      firstName: firstName,
+      lastName: lastName,
+      salary: parseFloat(salary), // Convert salary to a float
+    };
+    employees.push(employee); // Add the employee object to the array
+  }
+  return employees; // Return the array of employee objects
+  console.log(employees);
+};
+// ========================================================================== Average Salary
+// Display the average salary
+const displayAverageSalary = function (employeesArray) {
+  // TODO: Calculate and display the average salary
+  let totalSalary = 0; // Variable to store the total salary
+
+  // Loop through each employee in array
+  for (let employee of employeesArray) {
+    totalSalary += employee.salary; // Update total salary with each employee's salary
+  }
+
+  const averageSalary = totalSalary / employeesArray.length;
+  const displayAverage = document.querySelector("#average-salary"); // Display average salary in an element
+
+  // Do formatting on the salary
+  displayAverage.textContent =
+    "The average employee salary is: " +
+    averageSalary.toLocaleString("en-US", {
+      style: "currency",
+      currency: "USD",
+    });
+
+    // Print employee count and average salary
+    let countEmployees = employeesArray.length; 
+    // Keep this in one line to avoid odd space in dev console
+    console.log(`The average employee salary between our ${countEmployees} employees is: ${averageSalary.toLocaleString("en-US", { 
+      style: "currency",
+      currency: "USD",
+    })}`);
+
+  // I added <p id="average-salary></p>" in the HTML to get rid of the Uncaught TypeError 
+  // Uncaught TypeError: Cannot set properties of null (setting 'textContent')
+};
+
+// ========================================================================== Get Random Employees
+// Select a random employee
+const getRandomEmployee = function (employeesArray) {
+  // TODO: Select and display a random employee
+  let rondomEmployee = Math.floor(Math.random() * employeesArray.length); // Random for the employeesArray length
+  let newRandomEmployee = employeesArray[rondomEmployee];
+
+  console.log(`Congratulations to ${newRandomEmployee.firstName} ${newRandomEmployee.lastName}, our random draw winner!`);
+};
+
+/*
+  ========================================================================== STARTER CODE
+  Do not modify any of the code below this line:
+*/
+
+// Display employee data in an HTML table
+const displayEmployees = function (employeesArray) {
+  // Get the employee table
+  const employeeTable = document.querySelector("#employee-table");
+
+  // Clear the employee table
+  employeeTable.innerHTML = "";
+
+  // Loop through the employee data and create a row for each employee
+  for (let i = 0; i < employeesArray.length; i++) {
+    const currentEmployee = employeesArray[i];
+
+    const newTableRow = document.createElement("tr");
+
+    const firstNameCell = document.createElement("td");
+    firstNameCell.textContent = currentEmployee.firstName;
+    newTableRow.append(firstNameCell);
+
+    const lastNameCell = document.createElement("td");
+    lastNameCell.textContent = currentEmployee.lastName;
+    newTableRow.append(lastNameCell);
+
+    const salaryCell = document.createElement("td");
+    // Format the salary as currency
+    salaryCell.textContent = currentEmployee.salary.toLocaleString("en-US", {
+      style: "currency",
+      currency: "USD",
+    });
+
+    newTableRow.append(salaryCell);
+
+    employeeTable.append(newTableRow);
+  }
+};
+
+const trackEmployeeData = function () {
+  const employees = collectEmployees();
+
+  console.table(employees);
+
+  displayAverageSalary(employees);
+
+  console.log("==============================");
+
+  getRandomEmployee(employees);
+
+  employees.sort(function (a, b) {
+    if (a.lastName < b.lastName) {
+      return -1;
+    } else {
+      return 1;
+    }
+  });
+
+  displayEmployees(employees);
+};
+
+// Add event listener to 'Add Employees' button
+addEmployeesBtn.addEventListener("click", trackEmployeeData);
